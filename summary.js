5cc0bc9d071175a82a2ed84cdf45cd9736d51841
Mahmud2640 <mdmahmud484@gmail.com>
2022-02-02 18:05:02 +0600

    finish

---
+++ summary.js
@@ -0,0 +1,13 @@
+var phoneName ="iphone";
+
+var brandName = ['apple', 'saomi', 'lg', 'samsung'];
+
+// add some value 
+brandName[2] = 'walton';
+
+if (brandName.indexOf('kaltu') == -1) {
+   console.log('kaltu is mising');
+}
+if (brandName.indexOf('apple') != -1) {
+   console.log('apple is availbe');
+}
