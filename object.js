0370b66235859801b3c7b7ddb5ab0d062572e01a
Mahmud2640 <mdmahmud484@gmail.com>
2022-02-02 17:06:19 +0600

    add some code

---
+++ object.js
@@ -0,0 +1,26 @@
+var studen1 = {
+   id : 006,
+   name : 'Rahad',
+   age : 20,
+   tk : 400,
+   father : 'Abu'
+};
+var studen2 = {
+   id : 005,
+   name : 'Mahmud',
+   age : 30,
+   tk : 500,
+   father : 'Taher'
+};
+var studen3 = {
+   id : 007,
+   name : 'Nur Alom',
+   age : 60,
+   tk : 700,
+   father : 'Abul'
+};
+
+// console.log(studen3.father);
+// console.log(studen3.name);
+studen3.name = 'mahmud'
+console.log(studen3.name);
---
+++ switch.js
@@ -0,0 +1,18 @@
+var tomarNam = 'mahmud';
+switch (tomarNam) {
+   case 'mahmud' :
+      console.log('yeap this is me.');      
+      break;
+   case 'nurAlom' :
+      console.log('wrong person .');      
+      break;
+   case 'rahat' :
+      console.log('this is hujur.');      
+      break;
+   case 'khadija' :
+      console.log('yeap this is my sister .');      
+      break;
+
+   default:
+      console.log('kicu ektaaa typ koro');
+}
